648158c83a7b3d4783636fbfadda50cc8fa3c0e5
Joshua Clayton <jclayton@thoughtbot.com>
2016-01-11 16:11:11 -0500

    Display users of an org

--- spec/features/smoke_spec.rb
+++ spec/features/smoke_spec.rb
@@ -10,9 +10,25 @@ feature "Smoke test" do
     github = create(:organization, name: "GitHub")
     thoughtbot = create(:organization, name: "thoughtbot, inc.")
 
-    user = create(:user, name: "Joe", email: "person@example.com", organization: thoughtbot)
+    create(:user, email: "person@example.com", organization: thoughtbot)
 
     visit organization_path(github)
     expect(page).to have_css "header", text: /thoughtbot/
+  end
+
+  scenario "when a new user is added" do
+    github = create(:organization, name: "GitHub")
+    thoughtbot = create(:organization, name: "thoughtbot, inc.")
+
+    create(:user, email: "person@example.com", organization: thoughtbot)
+
+    visit organization_path(thoughtbot)
+    expect(page).to have_css "li", text: "person@example.com"
+
+    create(:user, email: "person1@example.com", organization: thoughtbot)
+
+    visit organization_path(thoughtbot)
+    expect(page).to have_css "li", text: "person@example.com"
+    expect(page).to have_css "li", text: "person1@example.com"
   end
 end
