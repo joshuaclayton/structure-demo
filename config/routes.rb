2859ab0a620e4d601463c7a199b0c79813d5e988
Joshua Clayton <jclayton@thoughtbot.com>
2016-01-11 15:55:58 -0500

    Add organizations page demonstrating failure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "profile#show"
+
+  resources :organizations, only: [:show]
 end
--- spec/features/smoke_spec.rb
+++ spec/features/smoke_spec.rb
@@ -5,4 +5,14 @@ feature "Smoke test" do
     visit root_path
     expect(page).to have_css ".title"
   end
+
+  scenario "works with organizations" do
+    github = create(:organization, name: "GitHub")
+    thoughtbot = create(:organization, name: "thoughtbot, inc.")
+
+    user = create(:user, name: "Joe", email: "person@example.com", organization: thoughtbot)
+
+    visit organization_path(github)
+    expect(page).to have_css "header", text: /thoughtbot/
+  end
 end
